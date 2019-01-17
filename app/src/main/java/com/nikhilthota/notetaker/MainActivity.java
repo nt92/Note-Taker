990494bfc7872f1c9fa809c6aef24651fb8355cb
Nikhil Thota <nt92@ufl.edu>
2019-01-16 21:44:11 -0500

    implemented google sign-in authentication

---
+++ app/src/main/java/com/nikhilthota/notetaker/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nikhilthota.notetaker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

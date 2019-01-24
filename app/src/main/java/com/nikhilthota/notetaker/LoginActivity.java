e64c0585b1f6dcf0c2bf09bbb15399ab7859f2e1
Nikhil Thota <nt92@ufl.edu>
2019-01-23 19:18:24 -0500

    implemented ability to save notes to firebase based on the user

--- app/src/main/java/com/nikhilthota/notetaker/LoginActivity.java
+++ app/src/main/java/com/nikhilthota/notetaker/LoginActivity.java
@@ -1,35 +1,12 @@
 package com.nikhilthota.notetaker;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -38,13 +15,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
- * A login screen that offers login via email/password.
+ * A login screen that offers login via Google
  */
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
@@ -74,12 +46,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if(account != null) {
             Log.v(TAG, "Already Logged in");
+            startMainActivity(account);
         }
     }
 
@@ -102,11 +74,20 @@ public class LoginActivity extends AppCompatActivity {
 
             // Signed in successfully, show authenticated UI.
             Log.v(TAG, "Signed in successfully");
+            startMainActivity(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
         }
+    }
+
+    private void startMainActivity(GoogleSignInAccount account){
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("account", account);
+        mainIntent.putExtras(bundle);
+        startActivity(mainIntent);
     }
 }
 
--- app/src/main/java/com/nikhilthota/notetaker/MainActivity.java
+++ app/src/main/java/com/nikhilthota/notetaker/MainActivity.java
@@ -1,19 +1,72 @@
 package com.nikhilthota.notetaker;
 
-import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The main activity which allows the user to save a note
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int RC_SIGN_IN = 1;
+
+    private EditText noteText;
+    private Button saveButton;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteText = (EditText) findViewById(R.id.note_text_content);
+        saveButton = (Button) findViewById(R.id.save_note_button);
+
+        Bundle extras = getIntent().getExtras();
+        String email = "";
+        if (extras != null) {
+            Log.v(TAG, "notnull");
+            email = extras.getString("email");
+        }
+
+        final String finalEmail = email;
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, Object> note = new HashMap<>();
+                note.put("user", finalEmail);
+                note.put("content", noteText.getText().toString());
+
+                // Add a new document with a generated ID
+                db.collection("notes")
+                        .add(note)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
     }
 }
